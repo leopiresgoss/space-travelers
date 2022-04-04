1058d7dd1fc595f1ed6bd9fc64a492a1f588deb8
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-04-04 11:06:57 -0600

    Add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Missions from './pages/Missions';
+import Profile from './pages/Profile';
+import Rockets from './pages/Rockets';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
