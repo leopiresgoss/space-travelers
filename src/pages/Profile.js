e6486820f75a3f8bd4dfa6091e5b65271367965a
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-04-05 16:48:07 -0500

    Implement reserved rockets in my profile
    modified:   src/components/NavBar/navbar.css
    new file:   src/components/Rockets/RocketsReserved.css
    new file:   src/components/Rockets/RocketsReserved.js
    modified:   src/pages/Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,14 @@
-const Profile = () => (
-  <h2>Profile</h2>
-);
+import { useSelector } from 'react-redux';
+import RocketsReserved from '../components/Rockets/RocketsReserved';
+
+const Profile = () => {
+  const rocketsList = useSelector((state) => state.rocketsReducer.rockets);
+  return (
+    <div className="flex">
+      <RocketsReserved rockets={rocketsList.filter((rocket) => rocket.reserved)} />
+      <RocketsReserved rockets={rocketsList.filter((rocket) => rocket.reserved)} />
+    </div>
+  );
+};
 
 export default Profile;
