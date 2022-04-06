d264d40a35cb6c8d28fe5bc49df7d8b6b679f67d
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-04-06 13:01:56 -0500

    Update readme file
    modified:   README.md
    modified:   public/favicon.ico
    new file:   spaceTravelers.png
    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
